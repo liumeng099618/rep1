9e5c5641d857c4c027d3d5a9bc3d4e5075420bdf
xiaoan <xiaoan@163.com>
2020-05-06 09:20:29 +0800

    l

--- day1/src/com/itheima/test/Test.java
+++ day1/src/com/itheima/test/Test.java
@@ -10,5 +10,7 @@ public class Test {
         System.out.println("hello  world");
         System.out.println("hello  world");
         System.out.println("hello  world");
+        System.out.println("hello  world");
+        System.out.println("hello  world");
     }
 }
