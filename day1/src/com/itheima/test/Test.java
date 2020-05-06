c2e1f107e98dfc21ed3793563d8e05d827a06ab9
xiaoan <xiaoan@163.com>
2020-05-06 09:15:41 +0800

    l

--- day1/src/com/itheima/test/Test.java
+++ day1/src/com/itheima/test/Test.java
@@ -6,6 +6,9 @@ package
  */
 public class Test {
     public static void main(String[] args) {
+
+        System.out.println("hello  world");
+        System.out.println("hello  world");
         System.out.println("hello  world");
     }
 }
