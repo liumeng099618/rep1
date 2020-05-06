1ef161b72351219ea418aa72adf91c1d75aa503d
xiaoan <xiaoan@163.com>
2020-05-06 08:38:06 +0800

    第一次提交

---
+++ day1/src/com/itheima/test/Test.java
@@ -0,0 +1,11 @@
+package
+        com.itheima.test;
+
+/**
+ * @author xuan
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("hello  world");
+    }
+}
